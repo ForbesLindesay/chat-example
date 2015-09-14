c958ca3d04ff861c4b54152c6915eef7c4fd46c4
Forbes Lindesay <forbes@lindesay.co.uk>
2015-09-14 21:28:56 +0100

    Fix s/get/read

--- bicycle/src/actions.js
+++ bicycle/src/actions.js
@@ -2,7 +2,7 @@ export const BICYCLE_REQUEST = 'BICYCLE_REQUEST';
 export const BICYCLE_SUCCESS = 'BICYCLE_SUCCESS';
 export const BICYCLE_FAILURE = 'BICYCLE_FAILURE';
 export function request(method, path, input, qs) {
-  if ((method === 'remove' || method === 'get') && input && !qs) {
+  if ((method === 'remove' || method === 'read') && input && !qs) {
     qs = input;
     input = null;
   }
